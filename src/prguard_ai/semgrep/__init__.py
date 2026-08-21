fee1849820ed3514229f808f93dc32f048502329
purvanshh <purvansh.23bcs10169@ms.sst.scaler.com>
2026-08-21 10:10:09 +0530

    fix: restore semgrep package exports in init

--- src/prguard_ai/semgrep/__init__.py
+++ src/prguard_ai/semgrep/__init__.py
@@ -1,1 +1,14 @@
 """Semgrep static analysis integration for PRGuard AI."""
+
+from prguard_ai.semgrep.agent import run_semgrep_scan, semgrep_enabled_for
+from prguard_ai.semgrep.parser import SemgrepFinding, findings_to_issues, parse_semgrep_json
+from prguard_ai.semgrep.scanner import SemgrepScanner
+
+__all__ = [
+    "SemgrepFinding",
+    "SemgrepScanner",
+    "findings_to_issues",
+    "parse_semgrep_json",
+    "run_semgrep_scan",
+    "semgrep_enabled_for",
+]
